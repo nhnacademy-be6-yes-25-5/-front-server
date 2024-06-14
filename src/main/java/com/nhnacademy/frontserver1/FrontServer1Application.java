fdcb05a595d52e16f41b996102b21c84a39884be
Hyeok000 <hyok000207@gmail.com>
2024-06-14 13:44:57 +0900

    front1 ci test

---
+++ src/main/java/com/nhnacademy/frontserver1/FrontServer1Application.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.frontserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontServer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontServer1Application.class, args);
+    }
+
+}

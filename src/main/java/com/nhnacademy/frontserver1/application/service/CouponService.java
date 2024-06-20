bdbf74f94339488a8d73891d9f240fa28f33409a
임채령 <sa990422@naver.com>
2024-06-21 00:01:40 +0900

    feat : 쿠폰정책목록, 생성, 페이징처리 구현

--- src/main/java/com/nhnacademy/frontserver1/application/service/CouponService.java
+++ src/main/java/com/nhnacademy/frontserver1/application/service/CouponService.java
@@ -1,12 +1,16 @@
 package com.nhnacademy.frontserver1.application.service;
 
-
+import com.nhnacademy.frontserver1.presentation.dto.request.coupon.CouponPolicyRequestDTO;
+import com.nhnacademy.frontserver1.presentation.dto.response.coupon.CouponPolicyResponseDTO;
 import com.nhnacademy.frontserver1.presentation.dto.response.coupon.CouponUserListResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface CouponService {
-    List<CouponUserListResponseDTO> findAllCoupons();
-    //List<CouponUserListResponseDTO> findUserCouponsByUserId(Long userId);
+    Page<CouponPolicyResponseDTO> findAllCouponPolicies(Pageable pageable);
     List<CouponUserListResponseDTO> findUserCoupons(Long userId);
+    void createCoupon(CouponPolicyRequestDTO createCouponRequest);
+    void deleteCoupon(Long id);
 }
--- src/main/java/com/nhnacademy/frontserver1/infrastructure/adaptor/CouponAdaptor.java
+++ src/main/java/com/nhnacademy/frontserver1/infrastructure/adaptor/CouponAdaptor.java
@@ -1,18 +1,25 @@
 package com.nhnacademy.frontserver1.infrastructure.adaptor;
 
+import com.nhnacademy.frontserver1.presentation.dto.request.coupon.CouponPolicyRequestDTO;
+import com.nhnacademy.frontserver1.presentation.dto.response.coupon.CouponPolicyResponseDTO;
 import com.nhnacademy.frontserver1.presentation.dto.response.coupon.CouponUserListResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @FeignClient(name = "couponAdaptor", url = "http://localhost:8085")
 public interface CouponAdaptor {
 
-    @GetMapping("/coupons")
-    List<CouponUserListResponseDTO> findAllCoupons();
+    @GetMapping("/admin-policy")
+    List<CouponPolicyResponseDTO> findAll();
 
     @GetMapping("/user-coupons/user")
     List<CouponUserListResponseDTO> findUserCoupons(@RequestParam("userId") Long userId);
+
+    @PostMapping("/admin-policy/coupon")
+    void create(@RequestBody CouponPolicyRequestDTO createCouponRequest);
+
+    @DeleteMapping("/admin-policy/coupon/{id}")
+    void delete(@PathVariable Long id);
 }

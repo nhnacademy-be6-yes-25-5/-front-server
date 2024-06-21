411a18670cd53f385bc3c629999e129450adf1f0
bbbbooo <qlqlzbxkzl@naver.com>
2024-06-21 16:26:23 +0900

    feat : 유저 장바구니를 조회하도록 수정함

--- src/main/java/com/nhnacademy/frontserver1/presentation/controller/OrderController.java
+++ src/main/java/com/nhnacademy/frontserver1/presentation/controller/OrderController.java
@@ -2,13 +2,12 @@ package com.nhnacademy.frontserver1.presentation.controller;
 
 import com.nhnacademy.frontserver1.application.service.OrderService;
 import com.nhnacademy.frontserver1.presentation.dto.request.order.CreateOrderRequest;
-import com.nhnacademy.frontserver1.presentation.dto.request.order.FindProductRequest;
+import com.nhnacademy.frontserver1.presentation.dto.request.order.ReadCartBookResponse;
 import com.nhnacademy.frontserver1.presentation.dto.response.order.CreateOrderResponse;
 import com.nhnacademy.frontserver1.presentation.dto.response.order.ReadShippingPolicyResponse;
 import com.nhnacademy.frontserver1.presentation.dto.response.order.ReadTakeoutResponse;
 import jakarta.servlet.http.HttpSession;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,29 +30,29 @@ public class OrderController {
 
     @GetMapping("checkout")
     public String findAllCheckout(Model model, Pageable pageable) {
-        List<FindProductRequest> requests = new ArrayList<>();
-        requests.add(new FindProductRequest(3L, 30, BigDecimal.valueOf(1000)));
-        requests.add(new FindProductRequest(4L, 1, BigDecimal.valueOf(5000)));
+        Long userId = 8L;
+        List<ReadCartBookResponse> cartBookResponses = orderService.findAllCartBok(userId);
 
-        Integer totalAmount = getTotalAmount(requests);
+        Integer totalAmount = getTotalAmount(cartBookResponses);
         ReadShippingPolicyResponse shippingPolicy = orderService.findAllOrderPolicy(pageable, totalAmount);
         ReadShippingPolicyResponse freeShippingPolicy = orderService.findFreePolicy();
         Integer freeShippingAmount = freeShippingPolicy.shippingPolicyMinAmount() - totalAmount;
         List<ReadTakeoutResponse> takeoutResponses = orderService.findAllTakeout();
 
+        model.addAttribute("userId", userId);
         model.addAttribute("shippingPolicy", shippingPolicy);
         model.addAttribute("freeShippingPolicy", freeShippingPolicy);
         model.addAttribute("freeShippingAmount", freeShippingAmount);
         model.addAttribute("totalAmount", totalAmount);
-        model.addAttribute("requests", requests);
+        model.addAttribute("cartBooks", cartBookResponses);
         model.addAttribute("takeouts", takeoutResponses);
 
         return "order/checkout";
     }
 
-    private Integer getTotalAmount(List<FindProductRequest> requests) {
-        BigDecimal totalAmount = requests.stream()
-            .map(request -> request.price().multiply(BigDecimal.valueOf(request.quantity())))
+    private Integer getTotalAmount(List<ReadCartBookResponse> cartBookResponses) {
+        BigDecimal totalAmount = cartBookResponses.stream()
+            .map(cartBookResponse -> cartBookResponse.bookPrice().multiply(BigDecimal.valueOf(cartBookResponse.cartBookQuantity())))
             .reduce(BigDecimal.ZERO, BigDecimal::add);
 
         return totalAmount.intValue();
@@ -62,8 +61,7 @@ public class OrderController {
     @PostMapping
     public ResponseEntity<CreateOrderResponse> create(@RequestBody CreateOrderRequest request, HttpSession session) {
         log.info("주문 요청이 들어왔습니다. {}", request.toString());
-        Long userId = 1L;
-        CreateOrderResponse response = orderService.createPreOrder(request, userId);
+        CreateOrderResponse response = orderService.createPreOrder(request);
 
         session.setAttribute("orderId", response.orderId());
         session.setAttribute("totalAmount", response.totalAmount());

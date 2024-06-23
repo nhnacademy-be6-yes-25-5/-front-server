203d94e225d4f36664f64ca8ad4271e9ccfd5c3f
bbbbooo <qlqlzbxkzl@naver.com>
2024-06-23 14:59:54 +0900

    refactor : 게이트웨이 주소를 동적으로 사용하도록 변경함

--- src/main/java/com/nhnacademy/frontserver1/infrastructure/adaptor/OrderAdaptor.java
+++ src/main/java/com/nhnacademy/frontserver1/infrastructure/adaptor/OrderAdaptor.java
@@ -7,8 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-//@FeignClient(name = "orderAdaptor", url = "http://133.186.153.195:8085/orders")
-@FeignClient(name = "orderAdaptor", url = "http://localhost:8085/orders")
+@FeignClient(name = "orderAdaptor", url = "${eureka.gateway}/orders")
 public interface OrderAdaptor {
 
     @PostMapping
--- src/main/java/com/nhnacademy/frontserver1/infrastructure/adaptor/PaymentAdaptor.java
+++ src/main/java/com/nhnacademy/frontserver1/infrastructure/adaptor/PaymentAdaptor.java
@@ -5,8 +5,8 @@ import com.nhnacademy.frontserver1.presentation.dto.response.order.CreatePayment
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-//@FeignClient(name = "paymentAdaptor", url = "http://133.186.153.195:8085/payments")
-@FeignClient(name = "paymentAdaptor", url = "http://localhost:8085/payments")
+
+@FeignClient(name = "paymentAdaptor", url = "${eureka.gateway}/payments")
 public interface PaymentAdaptor {
 
     @PostMapping("/confirm")

d9005983db5bbb39e7a8428e118340349857802f
임채령 <sa990422@naver.com>
2024-06-26 15:51:33 +0900

    refactor : 게이트웨이 DNS 해석 문제에 대한 테스트

--- src/test/java/com/nhnacademy/frontserver1/FrontServer1ApplicationTests.java
+++ src/test/java/com/nhnacademy/frontserver1/FrontServer1ApplicationTests.java
@@ -1,14 +1,14 @@
-package com.nhnacademy.frontserver1;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootTest
-class FrontServer1ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.nhnacademy.frontserver1;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//@SpringBootTest
+//class FrontServer1ApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}

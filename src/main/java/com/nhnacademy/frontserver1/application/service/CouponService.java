48acc81e574ce93244c23195a5c05bfc4e4dfb55
임채령 <sa990422@naver.com>
2024-06-29 23:36:43 +0900

    feat : 카테고리 id, 카테고리 명 받아서 특정 카테고리 쿠폰정책 목록, 생성 기능 구현

--- src/main/java/com/nhnacademy/frontserver1/application/service/CouponService.java
+++ src/main/java/com/nhnacademy/frontserver1/application/service/CouponService.java
@@ -1,8 +1,10 @@
 package com.nhnacademy.frontserver1.application.service;
 
-import com.nhnacademy.frontserver1.presentation.dto.request.book.CouponPolicyBookRequestDTO;
+import com.nhnacademy.frontserver1.presentation.dto.request.coupon.CouponPolicyBookRequestDTO;
+import com.nhnacademy.frontserver1.presentation.dto.request.coupon.CouponPolicyCategoryRequestDTO;
 import com.nhnacademy.frontserver1.presentation.dto.request.coupon.CouponPolicyRequestDTO;
 import com.nhnacademy.frontserver1.presentation.dto.response.coupon.CouponPolicyBookResponseDTO;
+import com.nhnacademy.frontserver1.presentation.dto.response.coupon.CouponPolicyCategoryResponseDTO;
 import com.nhnacademy.frontserver1.presentation.dto.response.coupon.CouponPolicyResponseDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,4 +14,6 @@ public interface CouponService {
     void createCoupon(CouponPolicyRequestDTO createCouponRequest);
     void createCouponPolicyBook(CouponPolicyBookRequestDTO createCouponPolicyBookRequest);
     Page<CouponPolicyBookResponseDTO> findAllBookCouponPolicies(Pageable pageable);
+    void createCouponPolicyCategory(CouponPolicyCategoryRequestDTO createCouponPolicyCategoryRequest);
+    Page<CouponPolicyCategoryResponseDTO> findAllCategoryCouponPolicies(Pageable pageable);
 }
--- src/main/java/com/nhnacademy/frontserver1/presentation/controller/CouponPolicyBookController.java
+++ src/main/java/com/nhnacademy/frontserver1/presentation/controller/CouponPolicyBookController.java
@@ -1,20 +1,15 @@
 package com.nhnacademy.frontserver1.presentation.controller;
 
 import com.nhnacademy.frontserver1.application.service.CouponService;
-import com.nhnacademy.frontserver1.infrastructure.adaptor.CouponAdaptor;
-import com.nhnacademy.frontserver1.presentation.dto.request.book.CouponPolicyBookRequestDTO;
+import com.nhnacademy.frontserver1.presentation.dto.request.coupon.CouponPolicyBookRequestDTO;
 import com.nhnacademy.frontserver1.presentation.dto.response.coupon.CouponPolicyBookResponseDTO;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 import static org.springframework.data.domain.PageRequest.*;
 

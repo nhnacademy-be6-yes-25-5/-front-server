6906cf212afaeb98d936dd9fb10a8c9aa99aba95
Hyeok000 <hyok000207@gmail.com>
2024-06-30 18:30:21 +0900

    feat : 포인트 정책 조회 기능 추가

--- src/main/java/com/nhnacademy/frontserver1/infrastructure/adaptor/PointPolicyAdaptor.java
+++ src/main/java/com/nhnacademy/frontserver1/infrastructure/adaptor/PointPolicyAdaptor.java
@@ -1,13 +1,27 @@
 package com.nhnacademy.frontserver1.infrastructure.adaptor;
 
-import com.nhnacademy.frontserver1.presentation.dto.response.user.PointPolicyResponse;
+import com.nhnacademy.frontserver1.presentation.dto.request.user.PointPolicyRequest;
+import com.nhnacademy.frontserver1.presentation.dto.response.point.PointPolicyResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "pointPolicyAdaptor", url = "${eureka.gateway}")
+@FeignClient(name = "pointPolicyAdaptor", url = "${eureka.gateway}/users")
 public interface PointPolicyAdaptor {
 
     @GetMapping("/admin/point-policies/{pointPolicyId}")
     PointPolicyResponse getPointPolicy(@PathVariable Long pointPolicyId);
+
+    @GetMapping("/admin/point-policies")
+    Page<PointPolicyResponse> getPointPolicies(Pageable pageable);
+
+    @PostMapping("/admin/point-policies")
+    PointPolicyResponse createPointPolicy(@RequestBody PointPolicyRequest pointPolicyRequest);
+
+    @PutMapping("/admin/point-policies/{pointPolicyId}")
+    void updatePointPolicy(@PathVariable Long pointPolicyId, @RequestBody PointPolicyRequest pointPolicyRequest);
+
+    @DeleteMapping("/admin/point-policies/{pointPolicyId}")
+    void deletePointPolicy(@PathVariable Long pointPolicyId);
 }

8eaff49e8586810a61bda928f240b157783b6f91
Hyeok000 <hyok000207@gmail.com>
2024-06-30 18:30:46 +0900

    feat : UserAdaptor url 수정

--- src/main/java/com/nhnacademy/frontserver1/infrastructure/adaptor/UserAdaptor.java
+++ src/main/java/com/nhnacademy/frontserver1/infrastructure/adaptor/UserAdaptor.java
@@ -3,29 +3,31 @@ package com.nhnacademy.frontserver1.infrastructure.adaptor;
 import com.nhnacademy.frontserver1.presentation.dto.request.user.CreateUserRequest;
 import com.nhnacademy.frontserver1.presentation.dto.request.user.DeleteUserRequest;
 import com.nhnacademy.frontserver1.presentation.dto.request.user.UpdateUserRequest;
-import com.nhnacademy.frontserver1.presentation.dto.response.user.PointPolicyResponse;
+import com.nhnacademy.frontserver1.presentation.dto.response.point.PointLogResponse;
 import com.nhnacademy.frontserver1.presentation.dto.response.user.UpdateUserResponse;
 import com.nhnacademy.frontserver1.presentation.dto.response.user.UserGradeResponse;
 import com.nhnacademy.frontserver1.presentation.dto.response.user.UserResponse;
 import com.nhnacademy.frontserver1.common.config.FeignClientConfig;
 import com.nhnacademy.frontserver1.presentation.dto.response.order.ReadOrderUserInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "userAdaptor", url = "${eureka.gateway}/users", configuration = FeignClientConfig.class)
 public interface UserAdaptor {
 
-    @PostMapping("/users/sign-up")
+    @PostMapping("/sign-up")
     UserResponse signUp(@RequestBody CreateUserRequest userRequest);
 
-    @GetMapping("/users")
+    @GetMapping()
     UserResponse findByUserId();
 
-    @PutMapping("/users")
+    @PutMapping
     UpdateUserResponse updateUser(@RequestBody UpdateUserRequest userRequest);
 
-    @DeleteMapping("/users")
+    @DeleteMapping
     void deleteUser(@RequestBody DeleteUserRequest userRequest);
 
     @GetMapping("/orders/info")
@@ -33,4 +35,8 @@ public interface UserAdaptor {
 
     @GetMapping("/grades")
     UserGradeResponse getUserGrade();
+
+    @GetMapping("/point-logs")
+    Page<PointLogResponse> getUserPointLogs(Pageable pageable);
+
 }

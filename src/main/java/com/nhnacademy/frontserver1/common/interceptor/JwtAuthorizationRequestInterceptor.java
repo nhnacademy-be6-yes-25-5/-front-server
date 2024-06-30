bc5e0ab45c2eb0864e8483c24b11e60e08a27efd
Hyeok000 <hyok000207@gmail.com>
2024-06-30 19:50:33 +0900

    feat : 회원 가입 경로 인증 인터셉터 제외

--- src/main/java/com/nhnacademy/frontserver1/common/interceptor/JwtAuthorizationRequestInterceptor.java
+++ src/main/java/com/nhnacademy/frontserver1/common/interceptor/JwtAuthorizationRequestInterceptor.java
@@ -39,7 +39,7 @@ public class JwtAuthorizationRequestInterceptor implements RequestInterceptor {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
         String path = request.getServletPath();
 
-        if (path.startsWith("/auth/login")) {
+        if (path.startsWith("/auth/login") || path.startsWith("/users/sign-up")) {
             return;
         }
 

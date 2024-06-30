abdcb566056ac98fe64a494bc98a140a6a23ee6d
bbbbooo <qlqlzbxkzl@naver.com>
2024-07-01 03:31:03 +0900

    chore : 구현하지 않는 기능 주석처리

--- src/main/java/com/nhnacademy/frontserver1/application/service/impl/UserServiceImpl.java
+++ src/main/java/com/nhnacademy/frontserver1/application/service/impl/UserServiceImpl.java
@@ -2,21 +2,16 @@ package com.nhnacademy.frontserver1.application.service.impl;
 
 import com.nhnacademy.frontserver1.application.service.UserService;
 import com.nhnacademy.frontserver1.infrastructure.adaptor.UserAdaptor;
-import com.nhnacademy.frontserver1.presentation.dto.response.user.ReadUserInfoResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import com.nhnacademy.frontserver1.infrastructure.adaptor.UsersAdaptor;
 import com.nhnacademy.frontserver1.presentation.dto.response.address.UserAddressResponse;
+import com.nhnacademy.frontserver1.presentation.dto.response.user.ReadUserInfoResponse;
 import com.nhnacademy.frontserver1.presentation.dto.response.user.UserResponse;
+import java.util.Collections;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
-import java.util.List;
 
 
 @Service
@@ -33,13 +28,16 @@ public class UserServiceImpl implements UserService {
         return ReadUserInfoResponse.fromTest();
     }
 
+    // fixme. 해당 기능 구현하지 않아 주석처리하였습니다.
     @Override
     public UserResponse getUserById(Long id) {
-        UserResponse user = userAdaptor.getUserById(id);
-        if (user == null) {
-            throw new RuntimeException("User not found");
-        }
-        return user;
+//        UserResponse user = userAdaptor.getUserById(id);
+//        if (user == null) {
+//            throw new RuntimeException("User not found");
+//        }
+//        return user;
+
+        return null;
     }
 
     @Override

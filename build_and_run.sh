dfb6186d8b186a3b10136f1599f98730e7402ca2
wnstj1548 <wnstj0614@naver.com>
2024-06-30 15:45:59 +0900

    feat : "환경변수 설정"

--- build_and_run.sh
+++ build_and_run.sh
@@ -10,10 +10,10 @@ docker rm front1-app-8040 || true
 
 # Run container for port 8040
 docker run -d -p 8040:8040 --name front1-app-8040 \
-  -e NAVER_ID = ${NAVER_ID} \
-  -e NAVER_SECRET = ${NAVER_SECRET} \
-  -e NHN_IMAGE_SECRET = ${NHN_IMAGE_SECRET} \
-  -e NHN_IMAGE_APPKEY = ${NHN_IMAGE_APPKEY} \
+  -e NAVER_ID=${NAVER_ID} \
+  -e NAVER_SECRET=${NAVER_SECRET} \
+  -e NHN_IMAGE_SECRET=${NHN_IMAGE_SECRET} \
+  -e NHN_IMAGE_APPKEY=${NHN_IMAGE_APPKEY} \
   front1-app-8040
 
 # Wait for 30 seconds
@@ -25,8 +25,8 @@ docker rm front1-app-8041 || true
 
 # Run container for port 8041
 docker run -d -p 8041:8041 --name front1-app-8041 \
-  -e NAVER_ID = ${NAVER_ID} \
-  -e NAVER_SECRET = ${NAVER_SECRET} \
-  -e NHN_IMAGE_SECRET = ${NHN_IMAGE_SECRET} \
-  -e NHN_IMAGE_APPKEY = ${NHN_IMAGE_APPKEY} \
+  -e NAVER_ID=${NAVER_ID} \
+  -e NAVER_SECRET=${NAVER_SECRET} \
+  -e NHN_IMAGE_SECRET=${NHN_IMAGE_SECRET} \
+  -e NHN_IMAGE_APPKEY=${NHN_IMAGE_APPKEY} \
   front1-app-8041

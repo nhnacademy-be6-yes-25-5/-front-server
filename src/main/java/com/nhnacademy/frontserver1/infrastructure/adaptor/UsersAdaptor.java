c8cfac75a726a3c060ed04668c9aee0d0ba842e5
wnstj1548 <wnstj0614@naver.com>
2024-07-01 13:19:18 +0900

    feat : "usersAdapter 수정"

--- src/main/java/com/nhnacademy/frontserver1/infrastructure/adaptor/UsersAdaptor.java
+++ src/main/java/com/nhnacademy/frontserver1/infrastructure/adaptor/UsersAdaptor.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "userAdaptor", url = "http://localhost:8085")
+@FeignClient(name = "usersAdaptor", url = "http://localhost:8085")
 public interface UsersAdaptor {
 
     @GetMapping("/users/{userId}")

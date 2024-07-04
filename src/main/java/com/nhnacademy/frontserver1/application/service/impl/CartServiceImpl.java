94ac0a2b2bdcddf347b6369b81be1f8c75a29ec8
bbbbooo <qlqlzbxkzl@naver.com>
2024-07-05 01:11:43 +0900

    feat : 장바구니 변경 및 삭제 구현

--- src/main/java/com/nhnacademy/frontserver1/application/service/impl/CartServiceImpl.java
+++ src/main/java/com/nhnacademy/frontserver1/application/service/impl/CartServiceImpl.java
@@ -3,7 +3,10 @@ package com.nhnacademy.frontserver1.application.service.impl;
 import com.nhnacademy.frontserver1.application.service.CartService;
 import com.nhnacademy.frontserver1.infrastructure.adaptor.CartAdaptor;
 import com.nhnacademy.frontserver1.presentation.dto.request.cart.CreateCartRequest;
+import com.nhnacademy.frontserver1.presentation.dto.request.cart.UpdateCartBookRequest;
 import com.nhnacademy.frontserver1.presentation.dto.response.cart.CreateCartResponse;
+import com.nhnacademy.frontserver1.presentation.dto.response.cart.DeleteCartBookResponse;
+import com.nhnacademy.frontserver1.presentation.dto.response.cart.UpdateCartBookResponse;
 import com.nhnacademy.frontserver1.presentation.dto.response.order.ReadCartBookResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -23,5 +26,15 @@ public class CartServiceImpl implements CartService {
     @Override
     public List<ReadCartBookResponse> getCarts() {
         return cartAdaptor.getCartBooks();
+    }
+
+    @Override
+    public UpdateCartBookResponse updateCart(Long bookId, UpdateCartBookRequest request) {
+        return cartAdaptor.updateCartBook(bookId, request);
+    }
+
+    @Override
+    public DeleteCartBookResponse deleteCartBook(Long bookId) {
+        return cartAdaptor.deleteCartBookByBookId(bookId);
     }
 }

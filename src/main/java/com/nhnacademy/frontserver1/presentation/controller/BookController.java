2ca8885c06d54e8d8d36a34aa64d7ea749c9061d
bbbbooo <qlqlzbxkzl@naver.com>
2024-07-04 16:35:43 +0900

    feat : 도서 상세 페이지 좋아요 비활성화

--- src/main/java/com/nhnacademy/frontserver1/presentation/controller/BookController.java
+++ src/main/java/com/nhnacademy/frontserver1/presentation/controller/BookController.java
@@ -28,9 +28,9 @@ public class BookController {
     public String detail(Model model, @PathVariable Long bookId) {
         BookResponse book = bookService.getBook(bookId);
 
-        if (likesAdapter.exist(bookId)) {
-            model.addAttribute("like", likesAdapter.findByBookIdAndUserId(bookId));
-        }
+//        if (likesAdapter.exist(bookId)) {
+//            model.addAttribute("like", likesAdapter.findByBookIdAndUserId(bookId));
+//        }
         List<Long> categoryIds = bookService.getCategoryIdsByBookId(bookId);
 //        List<BookDetailCouponResponseDTO> coupons = couponService.getCoupons(bookId, categoryIds);
 //        model.addAttribute("coupons", coupons);

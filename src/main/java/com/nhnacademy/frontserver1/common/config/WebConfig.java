8ce91672db4103b6b9d2e725351111966d4e6a14
bbbbooo <qlqlzbxkzl@naver.com>
2024-07-05 16:47:50 +0900

    feat : 도서 상세 페이지 리뷰 섹션 분리

--- src/main/java/com/nhnacademy/frontserver1/common/config/WebConfig.java
+++ src/main/java/com/nhnacademy/frontserver1/common/config/WebConfig.java
@@ -15,6 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(userInfoInterceptor)
-            .addPathPatterns("/mypage/**");
+            .addPathPatterns("/mypage/**")
+            .addPathPatterns("/detail/**");
     }
 }
--- src/main/java/com/nhnacademy/frontserver1/presentation/dto/response/user/ReadUserInfoResponse.java
+++ src/main/java/com/nhnacademy/frontserver1/presentation/dto/response/user/ReadUserInfoResponse.java
@@ -1,14 +1,4 @@
 package com.nhnacademy.frontserver1.presentation.dto.response.user;
 
-import lombok.Builder;
-
-@Builder
-public record ReadUserInfoResponse(Long gradeId, Integer points) {
-
-    public static ReadUserInfoResponse fromTest() {
-        return ReadUserInfoResponse.builder()
-            .gradeId(1L)
-            .points(50000)
-            .build();
-    }
+public record ReadUserInfoResponse(Long gradeId, Integer points, String name) {
 }

c8badeec4607371904ce7d2a7a63495208a7f80b
bbbbooo <qlqlzbxkzl@naver.com>
2024-07-07 22:23:45 +0900

    feat : 유저가 휴면일 경우, 휴면 페이지로 이동 구현 및 휴면 해제 구현

--- src/main/java/com/nhnacademy/frontserver1/presentation/controller/DormantController.java
+++ src/main/java/com/nhnacademy/frontserver1/presentation/controller/DormantController.java
@@ -1,12 +1,14 @@
 package com.nhnacademy.frontserver1.presentation.controller;
 
 import com.nhnacademy.frontserver1.application.service.AuthService;
-import com.nhnacademy.frontserver1.presentation.dto.response.dormant.CreateAuthNumberRequest;
+import com.nhnacademy.frontserver1.presentation.dto.request.dormant.SubmitAuthNumberRequest;
+import com.nhnacademy.frontserver1.presentation.dto.request.dormant.CreateAuthNumberRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,5 +29,11 @@ public class DormantController {
         authService.createAuthNumber(request);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/validate")
+    public ResponseEntity<Boolean> submitAuthNumber(@RequestBody SubmitAuthNumberRequest request) {
+
+        return ResponseEntity.ok(authService.submitAuthNumber(request));
     }
 }

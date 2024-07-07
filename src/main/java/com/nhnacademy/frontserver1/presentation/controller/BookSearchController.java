756276f11c740d789b54b94bc249b795083ab875
wnstj1548 <wnstj0614@naver.com>
2024-07-07 16:51:47 +0900

    fix : "Elastic Search 검색 구현 완료 및 페이징 처리"

--- src/main/java/com/nhnacademy/frontserver1/presentation/controller/BookSearchController.java
+++ src/main/java/com/nhnacademy/frontserver1/presentation/controller/BookSearchController.java
@@ -1,14 +1,19 @@
 package com.nhnacademy.frontserver1.presentation.controller;
 
+import com.nhnacademy.frontserver1.application.service.CategoryService;
 import com.nhnacademy.frontserver1.infrastructure.adaptor.BookSearchAdaptor;
 import com.nhnacademy.frontserver1.presentation.dto.request.book.BookSearchRequest;
 import com.nhnacademy.frontserver1.presentation.dto.response.book.BookIndexResponse;
+import com.nhnacademy.frontserver1.presentation.dto.response.book.CategoryResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Controller
@@ -16,46 +21,73 @@ import org.springframework.web.bind.annotation.*;
 public class BookSearchController {
 
     private final BookSearchAdaptor bookSearchAdaptor;
+    private final CategoryService categoryService;
 
     @GetMapping("/search")
-    public String search(Model model, @RequestParam String keyword, @RequestParam(name = "option") String option, Pageable pageable) {
+    public String search(Model model, @RequestParam String keyword, @RequestParam(name = "option") String option, @PageableDefault(size = 9, page = 0) Pageable pageable) {
+
+        Page<BookIndexResponse> bookIndexList;
+        List<CategoryResponse> rootCategories = categoryService.findRootCategories();
 
         if(option.equals("name")) {
 
-            Page<BookIndexResponse> bookIndexList = bookSearchAdaptor.searchByName(keyword, pageable);
+            bookIndexList = bookSearchAdaptor.searchByName(keyword, pageable);
 
             model.addAttribute("bookList", bookIndexList);
 
         } else if(option.equals("description")) {
 
-            Page<BookIndexResponse> bookIndexList = bookSearchAdaptor.searchByDescription(keyword, pageable);
+            bookIndexList = bookSearchAdaptor.searchByDescription(keyword, pageable);
 
             model.addAttribute("bookList", bookIndexList);
         } else if(option.equals("author")) {
 
-            Page<BookIndexResponse> bookIndexList = bookSearchAdaptor.searchByAuthorName(keyword, pageable);
+            bookIndexList = bookSearchAdaptor.searchByAuthorName(keyword, pageable);
 
             model.addAttribute("bookList", bookIndexList);
 
         } else if(option.equals("tag")) {
 
-            Page<BookIndexResponse> bookIndexList = bookSearchAdaptor.searchByTagName(keyword, pageable);
+            bookIndexList = bookSearchAdaptor.searchByTagName(keyword, pageable);
 
             model.addAttribute("bookList", bookIndexList);
 
         } else if(option.equals("category")) {
 
-            Page<BookIndexResponse> bookIndexList = bookSearchAdaptor.searchByCategoryName(keyword, pageable);
+            bookIndexList = bookSearchAdaptor.searchByCategoryName(keyword, pageable);
 
             model.addAttribute("bookList", bookIndexList);
         } else {
 
-            Page<BookIndexResponse> bookIndexList = bookSearchAdaptor.searchAll(keyword, pageable);
+            bookIndexList = bookSearchAdaptor.searchAll(keyword, pageable);
 
             model.addAttribute("bookList", bookIndexList);
 
         }
 
-        return "product/product-list";
+        int nowPage = bookIndexList.getNumber();
+        int startPage = Math.max(nowPage - 4, 0);
+        int endPage = Math.min(nowPage + 5, bookIndexList.getTotalPages() - 1);
+
+        if(bookIndexList.getTotalPages() <= 10) {
+            startPage = 0;
+            endPage = bookIndexList.getTotalPages() - 1;
+        } else {
+            if (startPage == 0) {
+                endPage = 9;
+            } else if (endPage == bookIndexList.getTotalPages() -1) {
+                startPage = bookIndexList.getTotalPages() - 10;
+            }
+        }
+
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("option", option);
+        model.addAttribute("nowPage", nowPage + 1);
+        model.addAttribute("startPage", startPage + 1);
+        model.addAttribute("endPage", endPage + 1);
+        model.addAttribute("totalPages", bookIndexList.getTotalPages());
+        model.addAttribute("categories", rootCategories);
+
+        return "product/product-search-list";
     }
 }

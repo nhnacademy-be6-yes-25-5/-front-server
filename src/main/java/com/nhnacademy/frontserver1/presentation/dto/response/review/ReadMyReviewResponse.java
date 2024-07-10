55cb0d58fc92fc4c9165d728a4ee36817a5bbb90
bbbbooo <qlqlzbxkzl@naver.com>
2024-07-10 15:02:44 +0900

    feat : 마이 페이지 내 리뷰 조회 구현

--- src/main/java/com/nhnacademy/frontserver1/presentation/dto/response/review/ReadMyReviewResponse.java
+++ src/main/java/com/nhnacademy/frontserver1/presentation/dto/response/review/ReadMyReviewResponse.java
@@ -1,11 +1,14 @@
 package com.nhnacademy.frontserver1.presentation.dto.response.review;
 
 import java.time.LocalDate;
+import java.util.List;
 
-public record ReadMyReviewResponse(LocalDate createdAt,
+public record ReadMyReviewResponse(Long reviewId,
+                                   LocalDate createdAt,
                                    String title,
                                    String content,
                                    Integer rating,
-                                   String bookName) {
+                                   String bookName,
+                                   List<String> reviewImages) {
 
 }

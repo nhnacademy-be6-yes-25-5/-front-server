9e554732662b4231ab53a7456d998184d962a48d
bbbbooo <qlqlzbxkzl@naver.com>
2024-07-23 20:50:56 +0900

    fix : ResponseEntity를 사용하지 않아 엑세스 토큰을 재발급받지 못하는 이슈 수정

--- src/main/java/com/nhnacademy/frontserver1/infrastructure/adaptor/UserCouponAdaptor.java
+++ src/main/java/com/nhnacademy/frontserver1/infrastructure/adaptor/UserCouponAdaptor.java
@@ -5,6 +5,7 @@ import com.nhnacademy.frontserver1.presentation.dto.response.order.ReadMaximumDi
 import com.nhnacademy.frontserver1.presentation.dto.response.order.ReadUserCouponResponse;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface UserCouponAdaptor {
 
     @GetMapping("/user-coupons")
-    List<ReadUserCouponResponse> getAllUserCoupons();
+    ResponseEntity<List<ReadUserCouponResponse>> getAllUserCoupons();
 
     @GetMapping("/user-coupons/max")
     ReadMaximumDiscountCouponResponse getMaxDiscountCouponByTotalAmount(
